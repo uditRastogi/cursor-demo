797ff6d05d7da114944749a7c83acadaca360fe4
Udit Rastogi <udit.rastogi4@gmail.com>
2025-05-30 15:00:21 +0530

    refactor: simplify Dockerfile and remove start.sh script for more reliable deployment

--- backend/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Starting application from $(pwd)"
-echo "Listing target directory:"
-ls -la target/
-
-echo "Starting Java application..."
-exec java -jar target/backend-0.0.1-SNAPSHOT.jar 
